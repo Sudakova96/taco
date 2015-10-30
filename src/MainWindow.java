2fa637ddb83a9d9745fb8e7a30f598c4752bcae6
Sudakova Lera <Sudakova96angarsk@mail.ru>
2015-10-30 12:58:34 +0800

    Login and MainWindow added.

---
+++ src/MainWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+/**
+ * Created by User08 on 30.10.15.
+ */
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super();
+
+        this.setVisible(true);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
